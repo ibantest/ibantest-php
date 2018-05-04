c4cf119b05d399c64b6f44bf112b8d277353fac1
IBANTEST <info@ibantest.com>
2018-05-05 01:20:32 +0200

    add example.php

---
+++ example.php
@@ -0,0 +1,28 @@
+<?php
+
+include "vendor/autoload.php";
+
+use Ibantest\Ibantest;
+
+$api = new Ibantest();
+$api->setToken('###your_api_token###');
+
+# get count of remaining credits
+$res = $api->remainingCredits();
+print_r($res);
+
+# validate IBAN
+$res = $api->validateIban('DE02600501010002034304');
+print_r($res);
+
+# calculate IBAN out of country code, bank code and account number
+$res = $api->calculateIban('AT', '12000', '703447144');
+print_r($res);
+
+# validate BIC
+$res = $api->validateBic('BFSWDE33BER');
+print_r($res);
+
+# find Bank by country code and bank code
+$res = $api->findBank('CH', '100');
+print_r($res);
